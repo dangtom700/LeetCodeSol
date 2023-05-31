44195bf908d85ff11002371b938dd6ca4936d531
Azalea1047 <azalea1047@gmail.com>
2023-05-31 17:59:22 +0000

    gave up on valid parenthesis 79/93

--- valid_parenthesis.cpp
+++ valid_parenthesis.cpp
@@ -12,9 +12,30 @@ Every close bracket has a corresponding open bracket of the same type.
 #include<iostream>
 #include<map>
 
-int main(){
-    std::string s = "()[]{}";
+bool answer(std::string s){
     std::map<char,int> parenthesis = {{'(',0}, {')',0}, {'[',0}, {']',0}, {'{',0}, {'}',0}};
+    bool Answer = false;
+    for(char letter: s){
+        parenthesis.at(letter) += 1;
+        if(letter == '('){
+            parenthesis.at(letter) == parenthesis.at(')')+1 ? Answer = true: Answer = false;
+        }
+        if(letter == '['){
+            parenthesis.at(letter) == parenthesis.at(']')+1 ? Answer = true: Answer = false;
+        }
+        if(letter == '{'){
+            parenthesis.at(letter) == parenthesis.at('}')+1 ? Answer = true: Answer = false;
+        }
+        if(Answer == false) return Answer;
+    }
+    return parenthesis.at('(') == parenthesis.at(')') 
+            && parenthesis.at('[') == parenthesis.at(']') 
+            && parenthesis.at('{') == parenthesis.at('}');
+}
+int main(){
+    std::string s = "(]";
     
+    bool Answer = answer(s);
+    std::cout << Answer;
     return 0;
 }
